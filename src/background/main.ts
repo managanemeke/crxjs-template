ebaa072af1865663913eff2835b1aaa13d9529b2
managanemeke <managanemeke@gmail.com>
2024-11-21 19:44:27 +0500

    feat: init commit

---
+++ src/background/main.ts
@@ -0,0 +1,9 @@
+import {Logger} from "./Logger";
+
+export class Main {
+    constructor() {
+        new Logger();
+    }
+}
+
+new Main();
---
+++ src/content/Logger.ts
@@ -0,0 +1,5 @@
+export class Logger {
+    constructor() {
+        console.log("content");
+    }
+}
