84cdddf029c969906c8fb7780047000a00dcff4d
managanemeke <managanemeke@gmail.com>
2024-11-27 14:58:52 +0500

    refactor: extract 'src/all/' directory

--- src/all/index.tsx
+++ src/all/index.tsx
@@ -1,6 +1,6 @@
 import "./index.css"
 import { StrictMode } from "react";
-import { default as App } from "./App";
+import { default as App } from "./app/App";
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("app")!).render(
