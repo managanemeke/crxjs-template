9eb8cb57e064ce334161c51fa657e596492290c5
managanemeke <managanemeke@gmail.com>
2024-11-28 17:24:34 +0500

    feat(src/all/app/App): create grid table div

--- src/all/app/App.tsx
+++ src/all/app/App.tsx
@@ -11,15 +11,39 @@ export default function App() {
   const [count, setCount] = useState(0);
   const handle = useFullScreenHandle();
   const canvasRef = useRef<ReactInfiniteCanvasHandle>();
+
+  const token = "e60f99519b4af7b4ac39d484b6b7b0c24d928c7b08d2869871f515105d633be2";
+  const request = 10643;
+  const allStructures = new Set([112]);
+
   return (
     <div className="app">
       <h1>Vite + React</h1>
-      <StructureRestrictionCheckbox
-        token={"e60f99519b4af7b4ac39d484b6b7b0c24d928c7b08d2869871f515105d633be2"}
-        request={10643}
-        currentStructures={new Set([116, 118])}
-        allStructures={new Set([112])}
-      />
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "auto auto auto",
+          gridTemplateRows: "auto auto",
+          gridAutoFlow: "column",
+        }}
+      >
+        <StructureRestrictionCheckbox
+          token={token}
+          request={request}
+          currentStructures={new Set([113, 114])}
+          allStructures={allStructures}
+        />
+        <StructureRestrictionCheckbox
+          token={token}
+          request={request}
+          currentStructures={new Set([115, 116])}
+          allStructures={allStructures}
+        />
+        <div>113, 114</div>
+        <div>115, 116</div>
+        <div>first</div>
+        <div>second</div>
+      </div>
       <Button onClick={() => setCount((count) => count + 1)}>{String(count)}</Button>
       <Button onClick={handle.enter}>fullscreen</Button>
       <FullScreen className="canvas" handle={handle}>
