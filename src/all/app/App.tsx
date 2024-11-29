1cd6ee48cf05054eb7a1ff664554e3cba03d604e
managanemeke <managanemeke@gmail.com>
2024-11-29 17:15:59 +0500

    refactor(src/all/app/App): make grid table dynamic

--- src/all/app/App.tsx
+++ src/all/app/App.tsx
@@ -15,34 +15,76 @@ export default function App() {
   const token = "e60f99519b4af7b4ac39d484b6b7b0c24d928c7b08d2869871f515105d633be2";
   const request = 9985;
 
+  interface Table {
+    length: number;
+    checkboxes: Array<Set<number>>;
+    links: Array<string>;
+    descriptions: Array<string>;
+  }
+
+  const table: Table = {
+    length: 3,
+    checkboxes: [
+      new Set([36, 37]),
+      new Set([38, 39]),
+      new Set([40, 41]),
+    ],
+    links: [
+      "36, 37",
+      "38, 39",
+      "40, 41",
+    ],
+    descriptions: [
+      "first",
+      "second",
+      "third",
+    ],
+  };
+
+  const template = (number: number): string => {
+    const array = [];
+    for (let i = 1; i <= number; i++) {
+      array.push("auto");
+    }
+    return array.join(" ");
+  };
+
+  const links = (array: Array<string>) => {
+    const links = [];
+    for (const link of array) {
+      links.push(<div>{link}</div>);
+    }
+    return links;
+  }
+
+  const descriptions = (array: Array<string>) => {
+    const descriptions = [];
+    for (const description of array) {
+      descriptions.push(<div>{description}</div>);
+    }
+    return descriptions;
+  }
+
   return (
     <div className="app">
       <h1>Vite + React</h1>
       <div
         style={{
           display: "grid",
-          gridTemplateColumns: "auto auto auto",
-          gridTemplateRows: "auto auto auto",
-          gridAutoFlow: "column"
+          gridAutoFlow: "column",
+          gridTemplateColumns: template(table.length),
+          gridTemplateRows: template(table.checkboxes.length)
         }}
       >
         {
           StructureRestrictionCheckboxes({
             token: token,
             request: request,
-            currentStructuresArray: [
-              new Set([36, 37]),
-              new Set([38, 39]),
-              new Set([40, 41])
-            ]
+            currentStructuresArray: table.checkboxes,
           })
         }
-        <div>36, 37</div>
-        <div>38, 39</div>
-        <div>40, 41</div>
-        <div>first</div>
-        <div>second</div>
-        <div>third</div>
+        { links(table.links) }
+        { descriptions(table.descriptions) }
       </div>
       <Button onClick={() => setCount((count) => count + 1)}>{String(count)}</Button>
       <Button onClick={handle.enter}>fullscreen</Button>
