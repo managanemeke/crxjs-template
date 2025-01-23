6173645fe41758192584016ccf4a2357feb7a266
managanemeke <managanemeke@gmail.com>
2025-01-23 17:16:16 +0500

    feat: add ahooks dependency

--- src/all/app/App.tsx
+++ src/all/app/App.tsx
@@ -1,13 +1,18 @@
 import { Button } from "antd";
 import { useRef } from "react";
+import { useFullscreen } from "ahooks";
 
 export default function App() {
   const ref = useRef<HTMLDivElement>(null);
 
+  const [, {toggleFullscreen}] = useFullscreen(ref);
+
   return (
     <div ref={ref} className="app">
       <h1>Test</h1>
-      <Button>
+      <Button
+        onClick={toggleFullscreen}
+      >
         toggle
       </Button>
     </div>
